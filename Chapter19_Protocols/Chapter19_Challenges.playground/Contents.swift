0050bade97870d9b8be1042d6de6cfff64be6f1e
Perry <chenzhipeng901105@gmail.com>
2015-11-23 17:50:03 +0800

    add

---
+++ Chapter19_Protocols/Chapter19_Challenges.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+protocol CanSwim {
+    
+}
+
+protocol CanFly {
+    
+}
+
+class Pet {
+    var needFed: Bool?
+}
+
+class GoldFish: CanSwim {
+    var needTank: Bool?
+}
